a52ef6d52458cdbc665659f1589aa2cc7dfc1247
Sushmitha287 <130962732+Sushmitha287@users.noreply.github.com>
2023-07-13 09:54:01 +0530

    LinearSearchExample.java

---
+++ LinearSearchExample.java
@@ -0,0 +1,47 @@
+package basic;
+
+import java.util.Scanner;
+
+public class LinearSearchExample {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the size of the array
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        // Input the elements of the array
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Input the target element to search
+        System.out.print("Enter the target element to search: ");
+        int target = scanner.nextInt();
+
+        // Perform linear search
+        int index = linearSearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+
+        scanner.close();
+    }
+}
+
